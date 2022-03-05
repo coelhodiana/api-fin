476078baa11b25e3ff28d3b245aad1813a1f3489
diana coelho <diana@vps.server.local>
2022-03-05 12:47:43 +0000

    feat: api com docker

---
+++ db/models/transacoes.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const TransacoesSchema = new mongoose.Schema({
+  valor: {
+    type: Number,
+    get: (v) => (v / 100).toFixed(2),
+    set: (v) => v * 100,
+    required: true,
+  },
+  tipo: {
+    type: String,
+    required: true,
+  },
+  descricao: {
+    type: String,
+  },
+  dataInclusao: {
+    type: String,
+    required: true,
+  },
+});
+
+const Transacoes = mongoose.model("Transacoes", TransacoesSchema);
+
+module.exports = { Transacoes };
+
---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+
+const app = express();
+
+const bodyParser = require("body-parser");
+
+const { mongoose } = require("./db/mongoose");
+
+const { Transacoes } = require("./db/models");
+
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send(`Hello World!`);
+});
+
+app.get("/transacoes", (req, res) => {
+  Transacoes.find({}).then((transacoes) => {
+    res.send(transacoes);
+  });
+});
+
+app.post("/transacoes", (req, res) => {
+  let valor = req.body.valor;
+  let tipo = req.body.tipo;
+  let descricao = req.body.descricao;
+  let dataInclusao = req.body.dataInclusao;
+
+  let newTransacao = new Transacoes({
+    valor,
+    tipo,
+    descricao,
+    dataInclusao,
+  });
+
+  newTransacao.save().then((transacaoDoc) => {
+    res.send(transacaoDoc);
+  });
+});
+
+app.get("/transacoes/:id", (req, res) => {
+  Transacoes.findOne({ _id: req.params.id }).then((transacoes) => {
+    res.send(transacoes);
+  });
+});
+
+app.patch("/transacoes/:id", (req, res) => {
+  Transacoes.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      $set: req.body,
+    }
+  ).then(() => {
+    res.sendStatus(200);
+  });
+});
+
+app.delete("/transacoes/:id", (req, res) => {
+  Transacoes.findByIdAndDelete({ _id: req.params.id }).then(
+    (removedTransacoesDoc) => {
+      res.send(removedTransacoesDoc);
+    }
+  );
+});
+
+app.listen(3000);
